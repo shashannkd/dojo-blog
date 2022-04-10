d291594cbd9693e2e8365da88c02def8e4e89a8a
shashannkd <shashannkd@msitprogram.net>
2022-04-10 20:21:33 +0530

    handleClick and useState

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,23 @@
+
+import { useState } from 'react'
+
 const Home = () => {
+    // non reactive variable 
+    // let name = 'mario'
+    // reactive variable
+    const [name, setName] = useState('mario')
+    const [age, setAge] = useState('25')
+    const handleClick = (e) => {
+        setName('luigi');
+        setAge(30)
+    }
+
     return (
-        <h2>Home Page</h2>
+        <div className="home">
+            <h2>Home Page</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click Me</button>
+        </div>
     );
 }
 
