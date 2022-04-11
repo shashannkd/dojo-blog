ce8ec382f64ffd31be8b76b8ed492f64cd76f759
shashannkd <shashannkd@msitprogram.net>
2022-04-11 13:50:22 +0530

    added home link to the logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
+            <h1><a href='/'>The Dojo Blog</a></h1>
             <Link to="/">Home</Link>
             <Link to="create">New Blog</Link>
         </nav>
