bf670f60f776ea6956e8f7118479fa1a20564ad2
shashannkd <shashannkd@msitprogram.net>
2022-04-11 14:26:10 +0530

    delete blog functionality

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams();
     const { data: blog, isLoading, error } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory()
+
+    const handleDelete = () => {
+        fetch("http://localhost:8000/blogs/" + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -15,11 +24,14 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written By {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete this Blog</button>
+                    <div>
+                        <a id="go-to-home-page" href="/">
+                            Go back to Home Page
+                        </a>
+                    </div>
                 </article>
             )}
-            <a href="/">
-                Go back to All Blogs
-            </a>
         </div>);
 }
 
